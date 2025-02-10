b3ab47c190c3299a8545cc16eb139853c5e7eda9
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-02-10 11:19:23 +0000

    added embed.py

--- Embedding/embed.py
+++ Embedding/embed.py
@@ -1,1 +1,8 @@
-from langchain_community.embeddings import OllamaEmbeeding
+from langchain_ollama import OllamaEmbeddings
+
+embeddings = OllamaEmbeddings(
+    model="deepseek-r1:1.5b",
+)
+
+embeed = embeddings.embed_query("hello world")
+print(len(embeed))
