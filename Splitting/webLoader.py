18d26a4ccbbca56e8bddfe2c13fd39cca13d718c
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-02-10 09:59:57 +0000

    Reorganized files into subfolders

---
+++ Splitting/webLoader.py
@@ -0,0 +1,8 @@
+from langchain_community.document_loaders import WebBaseLoader
+
+
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Wikipedia:Wiki_Game",)
+
+data = loader.load()
+print(data)
+
--- webLoader.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-loader = WebBaseLoader("https://en.wikipedia.org/wiki/Wikipedia:Wiki_Game")
-
-data = loader.load()
-print(data)
