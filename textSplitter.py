c2c4eb0288f790010e97f67bfc9c586b3fc93a85
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-02-10 09:07:56 +0000

    Updated TextSplitter with new technique

--- textSplitter.py
+++ textSplitter.py
@@ -1,9 +1,18 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 mydata= ""
 
 with open('split.txt','r') as file:
     mydata=file.read()
-text_split = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
-splittedText= text_split.split_text(mydata)
+
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+# text_split = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+# splittedText= text_split.split_text(mydata)
+# print(splittedText)
+
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter = CharacterTextSplitter(separator="\n\n", chunk_size=100, chunk_overlap=20)
+splittedText= splitter.split_text(mydata)
+
 print(splittedText)
